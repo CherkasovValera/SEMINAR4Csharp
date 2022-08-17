a92c6852f11540ad300ea20e0de374aef767c50a
Valeriy <cherkasov77@mail.ru>
2022-08-17 22:03:05 +0400

    Семинар4задача27

--- Program.cs
+++ Program.cs
@@ -1,16 +1,35 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
 // и возводит число A в натуральную степень B.
 // задача с целыми числами.
-Console.Clear();
-Console.WriteLine( "Ведите число А:   ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine( "Ведите число B:   ");
-int b = int.Parse(Console.ReadLine());
-Console.WriteLine($"{a} в степени {b} -> {Math.Pow(a,b)}");
 // Console.Clear();
-// Console.WriteLine("Введите пятизначное число:  ");
-// int number = Convert.ToInt32(Console.ReadLine());
-// if (number > 9999 && number < 100000)
+// Console.WriteLine("Ведите число А:   ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Ведите число B:   ");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine($"{a} в степени {b} -> {Math.Pow(a, b)}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+// int SumNums(int n)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= Math.Abs(n % 10); i++)
+//     {
+//         n = n % 10;
+//         sum += i;
+//     }
+//     return sum;
+// }
+// Console.Clear();
+// Console.WriteLine("Введите число:  ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine($"Сумма цифр = {SumNums(num)}");
+
+
+
+
+//if (number > 9999 && number < 100000)
 // {
 //     if (number/ 10000 == (number) % 10 && (number % 10000 - number % 1000) / 1000 == (number % 100 - number % 10) / 10)
 //     {
