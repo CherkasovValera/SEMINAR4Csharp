a442f9c1f611cae99d2abc8399ce53d3a10400d9
Valeriy <cherkasov77@mail.ru>
2022-08-17 22:59:58 +0400

    Семинар4Все

--- Program.cs
+++ Program.cs
@@ -27,44 +27,33 @@
 // Console.WriteLine($"Сумма цифр = {SumNums(num)}");
 
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
 
-//if (number > 9999 && number < 100000)
-// {
-//     if (number/ 10000 == (number) % 10 && (number % 10000 - number % 1000) / 1000 == (number % 100 - number % 10) / 10)
-//     {
-//         Console.WriteLine($"{number} <- Число Полиндром");
-//     }
+void FillArray(int[] massiv)
+{
+    int length = massiv.Length;
+    int index = 0;
+    while (index < length)
+    {
+        massiv[index] = new Random().Next(1, 10);
+        index++;
+    }
+    
+}
+void PrintArray(int[] mass)
+{
+    int count = mass.Length;
+    int position = 0;
+    Console.Write("[");
+    while (position < count)
+    {
+        Console.Write($"{mass[position]} ");
+        position++;
+    }
+    Console.Write("]");
+}
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
 
-//     else
-//     {
-//         Console.WriteLine("Данное число не полиндром!");
-//     }
-// }
-// else
-// {
-//     Console.WriteLine("Введите пятизначное число:  !");
-// }
-
-// while (number < 10000 || number > 99999)
-// {
-//     int number1 = Convert.ToInt32(Console.ReadLine());
-//     if (number1 < 10000 || number1 > 99999)
-//     {
-//         Console.WriteLine("Введите пятизначное число!");
-//     }
-//     else
-//     {
-//         if ((number1 - number1 % 10000) / 10000 == (number1) % 10 && (number1 % 10000 - number1 % 1000) / 1000 == (number1 % 100 - number1 % 10) / 10)
-//         {
-//             Console.WriteLine($"{number1} <- Число Полиндром");
-//             return;
-//         }
-
-//         else
-//         {
-//             Console.WriteLine("Данное число не полиндром!");
-//             return;
-//         }
-//     }
-// }
