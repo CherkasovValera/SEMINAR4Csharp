516608d71e0ee875cf67728feb25905b4483f73d
Valeriy <cherkasov77@mail.ru>
2022-08-19 00:13:25 +0400

    Семинар4правкацикл

--- Program.cs
+++ Program.cs
@@ -1,35 +1,35 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
 // и возводит число A в натуральную степень B.
 // задача с целыми числами.
-Console.Clear();
-Console.WriteLine("Ведите число А:   ");
-int a = int.Parse(Console.ReadLine());
-Console.WriteLine("Ведите число B:   ");
-int b = int.Parse(Console.ReadLine());
-if (b >= 0)
-{
-    int sum = 1;
-    int i = 0;
-    while (i < b)
-    {
-        sum = sum * a;
-        i++;
-    }
-    Console.WriteLine($"{a} в степени {b} -> {sum}");
-}
-else
-{
-    double pr = 1;
-    int j = 0;
-    while (j < Math.Abs(b))
-    {
-        pr = pr / a;
-        j++;
+// Console.Clear();
+// Console.WriteLine("Ведите число А:   ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Ведите число B:   ");
+// int b = int.Parse(Console.ReadLine());
+// if (b >= 0)
+// {
+//     int sum = 1;
+//     int i = 0;
+//     while (i < b)
+//     {
+//         sum = sum * a;
+//         i++;
+//     }
+//     Console.WriteLine($"{a} в степени {b} -> {sum}");
+// }
+// else
+// {
+//     double pr = 1;
+//     int j = 0;
+//     while (j < Math.Abs(b))
+//     {
+//         pr = pr / a;
+//         j++;
 
-    }
-    Console.WriteLine($"{a} в степени {b} -> {pr = pr:f4}");
+//     }
+//     Console.WriteLine($"{a} в степени {b} -> {pr = pr:f4}");
 
-}
+// }
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
